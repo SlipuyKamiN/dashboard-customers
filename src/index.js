9d0dcef2278aafb37f5ac000d3e8aff2b4db61e3
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-06-13 17:15:50 +0200

    v 1.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const refs = {
   tableBody: document.querySelector('[data-table-body]'),
+  sideBarLogo: document.querySelector('[data-toggle-side-bar]'),
+  sideBar: document.querySelector('[data-side-bar]'),
 };
 
 const customersData = [
@@ -12,60 +14,60 @@ const customersData = [
     active: true,
   },
   {
-    name: 'Jane Cooper',
-    company: 'Microsoft',
-    phone: '(225)555-0118',
-    email: 'jane@microsoft.com',
-    country: 'United States',
+    name: 'Floyd Miles',
+    company: 'Yahoo',
+    phone: '(205)555-0100',
+    email: 'floyd@yahoo.com',
+    country: 'Kiribati',
+    active: false,
+  },
+  {
+    name: 'Ronald Richards',
+    company: 'Adobe',
+    phone: '(302)555-0107',
+    email: 'roland@adobe.com',
+    country: 'Israel',
+    active: false,
+  },
+  {
+    name: 'Marvin McKinney',
+    company: 'Tesla',
+    phone: '(252)555-0126',
+    email: 'marvin@tesla.com',
+    country: 'Iran',
     active: true,
   },
   {
-    name: 'Jane Cooper',
-    company: 'Microsoft',
-    phone: '(225)555-0118',
-    email: 'jane@microsoft.com',
-    country: 'United States',
+    name: 'Jerome Bell',
+    company: 'Google',
+    phone: '(629)555-0129',
+    email: 'jerome@google.com',
+    country: 'Réunion',
     active: true,
   },
   {
-    name: 'Jane Cooper',
+    name: 'Katryn Murphy',
     company: 'Microsoft',
-    phone: '(225)555-0118',
-    email: 'jane@microsoft.com',
-    country: 'United States',
+    phone: '(406)555-0120',
+    email: 'katryn@microsoft.com',
+    country: 'Curaçao',
     active: true,
   },
   {
-    name: 'Jane Cooper',
-    company: 'Microsoft',
-    phone: '(225)555-0118',
-    email: 'jane@microsoft.com',
-    country: 'United States',
+    name: 'Jacob Jones',
+    company: 'Yahoo',
+    phone: '(208) 555-0112',
+    email: 'jacob@yahoo.com',
+    country: 'Brazil',
     active: true,
   },
   {
-    name: 'Jane Cooper',
-    company: 'Microsoft',
-    phone: '(225)555-0118',
-    email: 'jane@microsoft.com',
-    country: 'United States',
-    active: true,
-  },
-  {
-    name: 'Jane Cooper',
-    company: 'Microsoft',
-    phone: '(225)555-0118',
-    email: 'jane@microsoft.com',
-    country: 'United States',
-    active: true,
-  },
-  {
-    name: 'Jane Cooper',
-    company: 'Microsoft',
-    phone: '(225)555-0118',
-    email: 'jane@microsoft.com',
-    country: 'United States',
-    active: true,
+    name: 'Kristin Watson',
+    company: 'Facebook',
+    phone: '(704) 555-0127',
+    email: 'kristin@facebook.com',
+    country: 'Åland Islands',
+    active: false,
   },
 ];
 
@@ -81,11 +83,21 @@ const createMarkup = data => {
           <td>${phone}</td>
           <td>${email}</td>
           <td>${country}</td>
-          <td>${active ? 'Active' : 'Inactive'}</td>
+          <td>
+          <span class="status ${
+            active ? 'status--active' : 'status--inactive'
+          }">${active ? 'Active' : 'Inactive'}</span>
+          </td>
+
         </tr>
         `;
     })
     .join('');
 };
 
+const toggleSideBar = () => {
+  refs.sideBar.classList.toggle('open');
+};
+
+refs.sideBarLogo.addEventListener('click', toggleSideBar);
 refs.tableBody.insertAdjacentHTML('beforeend', createMarkup(customersData));
